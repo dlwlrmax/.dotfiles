a715c346143ca45fe09ef3c02bc1c7b6e04736e8
kienct <kiel.cao@gmail.com>
2024-05-02 10:25:33 +0700

    fix config

--- nvim/.config/nvim/lua/dlwlrmax/setup.lua
+++ nvim/.config/nvim/lua/dlwlrmax/setup.lua
@@ -366,31 +366,31 @@ local plugins = {
 			})
 		end,
 	},
-	{
-		"nvim-neorg/neorg",
-		build = ":Neorg sync-parsers",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("neorg").setup({
-				load = {
-					["core.defaults"] = {},
-					["core.concealer"] = {},
-					["core.qol.todo_items"] = {},
-					["core.dirman"] = {
-						config = {
-							workspaces = {
-								notes = "~/notes",
-							},
-							default_workspace = "notes",
-						},
-					},
-				},
-			})
-
-			vim.wo.foldlevel = 99
-			vim.wo.conceallevel = 2
-		end,
-	},
+	-- {
+	-- 	"nvim-neorg/neorg",
+	-- 	build = ":Neorg sync-parsers",
+	-- 	dependencies = { "nvim-lua/plenary.nvim" },
+	-- 	config = function()
+	-- 		require("neorg").setup({
+	-- 			load = {
+	-- 				["core.defaults"] = {},
+	-- 				["core.concealer"] = {},
+	-- 				["core.qol.todo_items"] = {},
+	-- 				["core.dirman"] = {
+	-- 					config = {
+	-- 						workspaces = {
+	-- 							notes = "~/notes",
+	-- 						},
+	-- 						default_workspace = "notes",
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 		})
+	--
+	-- 		vim.wo.foldlevel = 99
+	-- 		vim.wo.conceallevel = 2
+	-- 	end,
+	-- },
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -49,6 +49,9 @@ fi
 
 unset ZSH_AUTOSUGGEST_USE_ASYNC
 
+export EDITOR="nvim"
+export VISUAL="nvim"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
