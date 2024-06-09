299cc00036167844291e6134d83395036fecb2cf
kielcao <kiel.cao@gmail.com>
2024-06-09 20:29:11 +0700

    chore: config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,16 +1,16 @@
 function getOS()
-  -- ask LuaJIT first
-  if jit then
-    return jit.os
-  end
+	-- ask LuaJIT first
+	if jit then
+		return jit.os
+	end
 
-  -- Unix, Linux variants
-  local fh, err = assert(io.popen("uname -o 2>/dev/null", "r"))
-  if fh then
-    os_name = fh:read()
-  end
+	-- Unix, Linux variants
+	local fh, err = assert(io.popen("uname -o 2>/dev/null", "r"))
+	if fh then
+		os_name = fh:read()
+	end
 
-  return os_name or "Windows"
+	return os_name or "Windows"
 end
 
 -- Pull in the wezterm API
@@ -22,7 +22,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -32,33 +32,33 @@ config.color_scheme = "Catppuccin Macchiato"
 
 local currentOs = getOS()
 if currentOs == "Windows" then
-  config.default_domain = "WSL:Arch"
+	config.default_domain = "WSL:Arch"
 end
 
 -- Font scale factor
 local scale = 1
 if currentOs == "Windows" then
-  scale = 1.05
+	scale = 1.05
 end
 
 config.use_ime = true
 config.ime_preedit_rendering = "Builtin"
 
 config.font = wezterm.font_with_fallback({
-  { family = "JetBrainsMono Nerd Font", weight = "Medium", scale = scale },
-  { family = "FiraCode Nerd Font",      weight = "Medium", scale = scale },
-  { family = "Noto Sans JP",            scale = scale },
-  { family = "Noto Sans KR",            scale = scale },
-  { family = "Noto Sans",               scale = scale },
+	{ family = "JetBrainsMono Nerd Font", weight = "Medium", scale = scale },
+	{ family = "FiraCode Nerd Font", weight = "Medium", scale = scale },
+	{ family = "Noto Sans JP", scale = scale },
+	{ family = "Noto Sans KR", scale = scale },
+	{ family = "Noto Sans", scale = scale },
 })
 config.font_size = 10
 config.use_cap_height_to_scale_fallback_fonts = true
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 8,
-  right = 8,
-  top = 8,
-  bottom = 8,
+	left = 8,
+	right = 8,
+	top = 8,
+	bottom = 8,
 }
 config.line_height = 1.1
 -- Other useful config options:
@@ -66,29 +66,29 @@ config.use_dead_keys = false
 config.scrollback_lines = 3500
 config.disable_default_key_bindings = true
 if currentOs == "Windows" then
-  config.disable_default_key_bindings = false
+	config.disable_default_key_bindings = false
 end
 config.keys = {
+	{
+		key = "c",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.CopyTo("Clipboard"),
+	},
+	{
+		key = "v",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.PasteFrom("Clipboard"),
+	},
+	{
+		key = "r",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ReloadConfiguration,
+	},
   {
-    key = "c",
-    mods = "SHIFT|CTRL",
-    action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
-  },
-  {
-    key = "v",
-    mods = "SHIFT|CTRL",
-    action = wezterm.action.PasteFrom("Clipboard"),
-  },
-  {
-    key = "v",
-    mods = "SHIFT|CTRL",
-    action = wezterm.action.PasteFrom("PrimarySelection"),
-  },
-  {
-    key = "r",
-    mods = "SHIFT|CTRL",
-    action = wezterm.action.ReloadConfiguration,
-  },
+    key = "a",
+    mods = "CTRL",
+    action = wezterm.action.CopyMode 'MoveToStartOfLine'
+  }
 }
 config.adjust_window_size_when_changing_font_size = false
 --UI related config
@@ -96,10 +96,10 @@ config.window_background_opacity = 0.98
 
 config.enable_wayland = false
 if currentOs == "Windows" then
-  config.window_background_opacity = 0
-  config.win32_system_backdrop = "Mica"
-  config.window_close_confirmation = "NeverPrompt"
-  config.enable_wayland = false
+	config.window_background_opacity = 0
+	config.win32_system_backdrop = "Mica"
+	config.window_close_confirmation = "NeverPrompt"
+	config.enable_wayland = false
 end
 
 config.initial_rows = 42
@@ -108,9 +108,5 @@ config.warn_about_missing_glyphs = false
 
 -- Keybindings
 local act = wezterm.action
--- local keys = {
---   { key = "R", mods = "SHIFT|CTRL", action = act.ReloadConfiguration },
--- }
 
--- and finally, return the configuration to wezterm
 return config
