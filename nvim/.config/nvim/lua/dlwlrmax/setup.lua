eb122aa96b6c94ae418e3531b60535bae9989171
kielcao <kiel.cao@gmail.com>
2024-09-17 18:44:02 +0700

    config

--- nvim/.config/nvim/lua/dlwlrmax/setup.lua
+++ nvim/.config/nvim/lua/dlwlrmax/setup.lua
@@ -476,14 +476,6 @@ local plugins = {
 		end,
 	},
 	{
-		"m4xshen/hardtime.nvim",
-		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-		opts = {
-      disable_mouse = false,
-      disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
-    },
-	},
-	{
 		"utilyre/barbecue.nvim",
 		name = "barbecue",
 		version = "*",
