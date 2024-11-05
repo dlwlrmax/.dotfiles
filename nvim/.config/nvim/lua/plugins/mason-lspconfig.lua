7e05ddd11340550e14f8069313b98702a9bcfdfd
kielcao <kiel.cao@gmail.com>
2024-11-05 20:42:48 +0700

    config

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,125 +1,135 @@
 return {
-	"williamboman/mason-lspconfig.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"dmmulroy/ts-error-translator.nvim",
+		config = function()
+			require("ts-error-translator").setup({
+				auto_override_publish_diagnostics = true,
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		require("mason-lspconfig").setup({
-			ensure_installed = { "ts_ls", "intelephense", "typos_lsp", "volar" },
-		})
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				if server_name == "tsserver" then
-					server_name = "ts_ls"
-				end
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-					-- on_attach = on_attach
-				})
-			end,
-			["intelephense"] = function()
-				local lspconfig = require("lspconfig")
-				lspconfig.intelephense.setup({
-					settings = {
-						intelephense = {
-							maxSize = 1000000,
+			require("mason-lspconfig").setup({
+				ensure_installed = { "ts_ls", "intelephense", "typos_lsp", "volar" },
+			})
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					if server_name == "tsserver" then
+						server_name = "ts_ls"
+					end
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+						-- on_attach = on_attach
+					})
+				end,
+				["intelephense"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.intelephense.setup({
+						settings = {
+							intelephense = {
+								maxSize = 1000000,
+							},
 						},
-					},
-				})
-			end,
-			["typos_lsp"] = function()
-				local lspconfig = require("lspconfig")
-				lspconfig.typos_lsp.setup({
-					init_options = {
-						config = "~/.config/nvim/typos.toml",
-					},
-				})
-			end,
-			["volar"] = function()
-				require("lspconfig").volar.setup({
-					-- NOTE: Uncomment to enable volar in file types other than vue.
-					-- (Similar to Takeover Mode)
-
-					-- filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact", "json" },
-
-					-- NOTE: Uncomment to restrict Volar to only Vue/Nuxt projects. This will enable Volar to work alongside other language servers (tsserver).
-
-					root_dir = require("lspconfig").util.root_pattern(
-						"vite.config.js",
-						"vue.config.js",
-						"vue.config.ts",
-						"nuxt.config.js",
-						"nuxt.config.ts"
-					),
-					init_options = {
-						vue = {
-							hybridMode = true,
+					})
+				end,
+				["typos_lsp"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.typos_lsp.setup({
+						init_options = {
+							config = "~/.config/nvim/typos.toml",
 						},
-						-- NOTE: This might not be needed. Uncomment if you encounter issues.
+					})
+				end,
+				["volar"] = function()
+					require("lspconfig").volar.setup({
+						-- NOTE: Uncomment to enable volar in file types other than vue.
+						-- (Similar to Takeover Mode)
 
-						-- typescript = {
-						--   tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
-						-- },
-					},
-					settings = {
-						typescript = {
-							inlayHints = {
-								enumMemberValues = {
-									enabled = true,
-								},
-								functionLikeReturnTypes = {
-									enabled = true,
-								},
-								propertyDeclarationTypes = {
-									enabled = true,
-								},
-								parameterTypes = {
-									enabled = true,
-									suppressWhenArgumentMatchesName = true,
-								},
-								variableTypes = {
-									enabled = true,
+						-- filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact", "json" },
+
+						-- NOTE: Uncomment to restrict Volar to only Vue/Nuxt projects. This will enable Volar to work alongside other language servers (tsserver).
+
+						root_dir = require("lspconfig").util.root_pattern(
+							"vite.config.js",
+							"vue.config.js",
+							"vue.config.ts",
+							"nuxt.config.js",
+							"nuxt.config.ts"
+						),
+						init_options = {
+							vue = {
+								hybridMode = true,
+							},
+							-- NOTE: This might not be needed. Uncomment if you encounter issues.
+
+							-- typescript = {
+							--   tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+							-- },
+						},
+						settings = {
+							typescript = {
+								inlayHints = {
+									enumMemberValues = {
+										enabled = true,
+									},
+									functionLikeReturnTypes = {
+										enabled = true,
+									},
+									propertyDeclarationTypes = {
+										enabled = true,
+									},
+									parameterTypes = {
+										enabled = true,
+										suppressWhenArgumentMatchesName = true,
+									},
+									variableTypes = {
+										enabled = true,
+									},
 								},
 							},
 						},
-					},
-				})
-			end,
+					})
+				end,
 
-			["ts_ls"] = function()
-				local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
-				local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
+				["ts_ls"] = function()
+					local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
+					local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
 
-				require("lspconfig").ts_ls.setup({
-					-- NOTE: To enable hybridMode, change HybrideMode to true above and uncomment the following filetypes block.
-					-- WARN: THIS MAY CAUSE HIGHLIGHTING ISSUES WITHIN THE TEMPLATE SCOPE WHEN TSSERVER ATTACHES TO VUE FILES
+					require("lspconfig").ts_ls.setup({
+						-- NOTE: To enable hybridMode, change HybrideMode to true above and uncomment the following filetypes block.
+						-- WARN: THIS MAY CAUSE HIGHLIGHTING ISSUES WITHIN THE TEMPLATE SCOPE WHEN TSSERVER ATTACHES TO VUE FILES
 
-					filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-					init_options = {
-						plugins = {
-							{
-								name = "@vue/typescript-plugin",
-								location = volar_path,
-								languages = { "vue" },
+						filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+						init_options = {
+							plugins = {
+								{
+									name = "@vue/typescript-plugin",
+									location = volar_path,
+									languages = { "vue" },
+								},
 							},
 						},
-					},
-					settings = {
-						typescript = {
-							inlayHints = {
-								includeInlayParameterNameHints = "all",
-								includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHints = true,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
+						settings = {
+							typescript = {
+								inlayHints = {
+									includeInlayParameterNameHints = "all",
+									includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+									includeInlayFunctionParameterTypeHints = true,
+									includeInlayVariableTypeHints = true,
+									includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+									includeInlayPropertyDeclarationTypeHints = true,
+									includeInlayFunctionLikeReturnTypeHints = true,
+									includeInlayEnumMemberValueHints = true,
+								},
 							},
 						},
-					},
-				})
-			end,
-		})
-	end,
+					})
+				end,
+			})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -26,6 +26,11 @@ return {
 		-- 	paths = { "~/.config/nvim/lua/snippets/common.lua" },
 		-- }) -- working on this
 		require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/lua/snippets/vs_code" })
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
 
 		local kind_icons = {
 			Text = "󰊄 [Text]",
@@ -108,28 +113,24 @@ return {
 					end
 				end, { "i", "s" }),
 
-				-- ["<CR>"] = cmp.mapping(function(fallback)
-				-- 	if cmp.visible() then
-				-- 		if luasnip.expandable() then
-				-- 			luasnip.expand()
-				-- 		else
-				-- 			cmp.confirm({
-				-- 				select = true,
-				-- 			})
-				-- 		end
-				-- 	else
-				-- 		fallback()
-				-- 	end
-				-- end),
-				["<CR>"] = cmp.mapping.confirm({
-					select = true,
-				}),
-
+				["<CR>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.confirm({
+							select = true,
+						})
+					else
+						fallback()
+					end
+				end),
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
 					elseif luasnip.locally_jumpable(1) then
 						luasnip.jump(1)
+					elseif has_words_before() then
+						cmp.complete()
 					else
 						fallback()
 					end
