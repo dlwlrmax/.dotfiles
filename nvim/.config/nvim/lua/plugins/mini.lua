9bc40984d47082b784aa71506135a6efe67b902c
kienct <kienct@langtech.vn>
2025-04-04 13:50:03 +0700

    feat(mini): add surround

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -42,6 +42,27 @@ return {
 			require("mini.cursorword").setup({
 				delay = 100,
 			})
+
+			require("mini.surround").setup({
+				mappings = {
+					add = "ya", -- Add surrounding in Normal and Visual modes
+					delete = "yd", -- Delete surrounding
+					find = "yf", -- Find surrounding (to the right)
+					find_left = "yF", -- Find surrounding (to the left)
+					highlight = "yh", -- Highlight surrounding
+					replace = "yr", -- Replace surrounding
+					update_n_lines = "yn", -- Update `n_lines`
+
+					suffix_last = "l", -- Suffix to search with "prev" method
+					suffix_next = "n", -- Suffix to search with "next" method
+				},
+			})
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>ya",
+				"yaiw",
+				{ noremap = false, silent = true, desc = "Quick surround WORD" }
+			)
 		end,
 	},
 	{
