f47b61c33357a62da472ba564b71ee64eeefc92f
kienct <kienct@langtech.vn>
2025-08-18 09:59:56 +0700

    fix mini.files

--- nvim/.config/nvim/lua/plugins/mini.files.lua
+++ nvim/.config/nvim/lua/plugins/mini.files.lua
@@ -10,4 +10,21 @@ return {
       desc = "Open mini.files (Directory of Current File)",
     },
   },
+  opts = {
+    mappings = {
+      close = "q",
+      go_in = "",
+      go_in_plus = "L",
+      go_out = "",
+      go_out_plus = "H",
+      mark_goto = "'",
+      mark_set = "m",
+      reset = "<BS>",
+      reveal_cwd = "@",
+      show_help = "g?",
+      synchronize = "<CR>",
+      trim_left = "<",
+      trim_right = ">",
+    },
+  },
 }
