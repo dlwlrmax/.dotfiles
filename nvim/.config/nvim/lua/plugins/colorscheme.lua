7dcd458609da191ddf02c636fec049b8a5a381e4
kienct <kienct@langtech.vn>
2025-08-28 08:37:08 +0700

    fix

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,16 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
-    }
-  }
+    },
+  },
+  {
+    "catppuccin/nvim",
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+  },
 }
