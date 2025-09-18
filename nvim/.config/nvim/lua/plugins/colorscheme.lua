52f4f11b32ff7089555d471da34f074f1f6599a7
kienct <kienct@langtech.vn>
2025-09-18 10:16:06 +0700

    update

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -6,13 +6,27 @@ return {
     },
   },
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
     "catppuccin/nvim",
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
+    lazy = false,
+    priority = 1000,
+    opts = {
+      term_colors = true,
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.overlay0 },
+          NeoTreeDotfile = { fg = colors.overlay0 },
+          NeoTreeMessage = { fg = colors.surface2 },
+          SnacksPickerListCursorLine = { bg = "#223547" },
+          SnacksPickerSelected = { fg = colors.lavender },
+        }
+      end,
+      auto_integrations = true,
+    },
   },
 }
