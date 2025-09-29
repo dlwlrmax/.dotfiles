6d31723d5d004ba6978f41eee8df17225b2412c6
kienct <kienct@langtech.vn>
2025-09-29 09:22:19 +0700

    config default

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,17 @@
+return {
+  "mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shellcheck",
+      "eslint-lsp",
+      "intelephense",
+      "pint",
+      "prettier",
+      "prettierd",
+      "tailwindcss-language-server",
+      "vue-language-server",
+      "vtsls"
+    }
+  }
+}
---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "cpp",
+      "css",
+      "html",
+      "javascript",
+      "php",
+      "vue",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+  },
+}
