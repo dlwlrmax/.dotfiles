79e33e9f95e7d0755c87704b1c17ad867bfc2ce7
kienct <kienct@langtech.vn>
2025-10-23 08:46:37 +0700

    handle hlargs

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,25 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- handle Hlargs Lsp semantic tokens
+vim.api.nvim_create_augroup("LspAttach_hlargs", { clear = true })
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "LspAttach_hlargs",
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then
+      return
+    end
+    local caps = client.server_capabilities
+    if not caps then
+      return
+    end
+    if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
+      require("hlargs").disable_buf(args.buf)
+    end
+  end,
+})
