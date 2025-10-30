81703e960b5904aa8a361efeeb50ec90da575f27
kienct <kienct@langtech.vn>
2025-10-30 10:39:38 +0700

    feat(nvim): add numbered completion selection to blink plugin

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -59,13 +59,58 @@ return {
       keymap = {
         preset = "default",
         ["<C-o>"] = { "select_and_accept" },
+        ["<A-1>"] = {
+          function(cmp)
+            cmp.accept({ index = 1 })
+          end,
+        },
+        ["<A-2>"] = {
+          function(cmp)
+            cmp.accept({ index = 2 })
+          end,
+        },
+        ["<A-3>"] = {
+          function(cmp)
+            cmp.accept({ index = 3 })
+          end,
+        },
+        ["<A-4>"] = {
+          function(cmp)
+            cmp.accept({ index = 4 })
+          end,
+        },
+        ["<A-5>"] = {
+          function(cmp)
+            cmp.accept({ index = 5 })
+          end,
+        },
+        ["<A-6>"] = {
+          function(cmp)
+            cmp.accept({ index = 6 })
+          end,
+        },
+        ["<A-7>"] = {
+          function(cmp)
+            cmp.accept({ index = 7 })
+          end,
+        },
+        ["<A-8>"] = {
+          function(cmp)
+            cmp.accept({ index = 8 })
+          end,
+        },
+        ["<A-9>"] = {
+          function(cmp)
+            cmp.accept({ index = 9 })
+          end,
+        },
       },
       completion = {
         menu = {
           border = "rounded",
           auto_show = true,
           draw = {
-            columns = {{ "kind_icon", "label", gap = 1 }, { "source_name", "kind", gap = 1 } },
+            columns = { { "item_idx" }, { "kind_icon", "label", gap = 1 }, { "source_name", "kind", gap = 1 } },
             components = {
               label = {
                 width = { fill = true, max = 60 },
@@ -88,6 +133,12 @@ return {
                   end
                   return highlights
                 end,
+              },
+              item_idx = {
+                text = function(ctx)
+                  return tostring(ctx.idx)
+                end,
+                highlight = "BlinkCmpItemIdx", -- optional, only if you want to change its color
               },
             },
           },
