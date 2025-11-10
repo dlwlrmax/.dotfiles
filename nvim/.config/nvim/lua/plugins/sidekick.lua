63801e23321247bba1b236dcdc5ee025e3eaabcb
kienct <kienct@langtech.vn>
2025-11-10 08:40:27 +0700

    feat(waybar): update clock format to day-month order

--- nvim/.config/nvim/lua/plugins/sidekick.lua
+++ nvim/.config/nvim/lua/plugins/sidekick.lua
@@ -1,37 +1,46 @@
 return {
   "folke/sidekick.nvim",
-  opts = {
-    nes = {
-      enabled = false,
-    },
-    copilot = {
-      status = {
+  opts = function(_, opts)
+    local setting = {
+      nes = {
         enabled = false,
       },
-    },
-    cli = {
-      mux = {
-        enabled = true,
-        backend = "tmux",
-      },
-      tools = {
-        opencode = {
-          cmd = { "opencode" },
-          -- HACK: https://github.com/sst/opencode/issues/445
-          env = { OPENCODE_THEME = "system" },
+      copilot = {
+        status = {
+          enabled = false,
         },
-        qwen = { cmd = { "qwen" } },
-        gemini = { cmd = { "gemini" } },
       },
-      prompts = {
-        refactor = "Please refactor {this} to be more maintainable",
-        security = "Review {file} for security vulnerabilities",
-        custom = function(ctx)
-          return "Current file: " .. ctx.buf .. " at line " .. ctx.row
-        end,
+      cli = {
+        mux = {
+          enabled = true,
+          backend = "tmux",
+        },
+        win = {
+          split = {
+            width = 90,
+            height = 20,
+          },
+        },
+        tools = {
+          opencode = {
+            cmd = { "opencode" },
+            -- HACK: https://github.com/sst/opencode/issues/445
+            env = { OPENCODE_THEME = "system" },
+          },
+          qwen = { cmd = { "qwen" } },
+          gemini = { cmd = { "gemini" } },
+        },
+        prompts = {
+          refactor = "Please refactor {this} to be more maintainable",
+          security = "Review {file} for security vulnerabilities",
+          custom = function(ctx)
+            return "Current file: " .. ctx.buf .. " at line " .. ctx.row
+          end,
+        },
       },
-    },
-  },
+    }
+    return vim.tbl_deep_extend("force", opts, setting)
+  end,
   keys = {
     {
       "<leader>aa",
