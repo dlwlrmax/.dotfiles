cf216ddb80f63227ac097f7b9b6bbc31fe1acd23
kienct <kienct@langtech.vn>
2026-01-13 09:01:49 +0700

    feat(nvim): remove blink completion number keybindings and item index display

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -60,51 +60,6 @@ return {
       local keymap = {
         preset = "default",
         ["<C-o>"] = { "select_and_accept" },
-        ["<A-1>"] = {
-          function(cmp)
-            cmp.accept({ index = 1 })
-          end,
-        },
-        ["<A-2>"] = {
-          function(cmp)
-            cmp.accept({ index = 2 })
-          end,
-        },
-        ["<A-3>"] = {
-          function(cmp)
-            cmp.accept({ index = 3 })
-          end,
-        },
-        ["<A-4>"] = {
-          function(cmp)
-            cmp.accept({ index = 4 })
-          end,
-        },
-        ["<A-5>"] = {
-          function(cmp)
-            cmp.accept({ index = 5 })
-          end,
-        },
-        ["<A-6>"] = {
-          function(cmp)
-            cmp.accept({ index = 6 })
-          end,
-        },
-        ["<A-7>"] = {
-          function(cmp)
-            cmp.accept({ index = 7 })
-          end,
-        },
-        ["<A-8>"] = {
-          function(cmp)
-            cmp.accept({ index = 8 })
-          end,
-        },
-        ["<A-9>"] = {
-          function(cmp)
-            cmp.accept({ index = 9 })
-          end,
-        },
       }
       local completion = {
         menu = {
@@ -112,7 +67,6 @@ return {
           auto_show = true,
           draw = {
             columns = {
-              { "item_idx" },
               { "kind_icon", "label", "label_description", gap = 1 },
               { "source_name", "kind", gap = 1 },
             },
@@ -138,12 +92,6 @@ return {
                   end
                   return highlights
                 end,
-              },
-              item_idx = {
-                text = function(ctx)
-                  return tostring(ctx.idx)
-                end,
-                highlight = "BlinkCmpItemIdx", -- optional, only if you want to change its color
               },
             },
           },
