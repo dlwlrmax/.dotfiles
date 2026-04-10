1af25bb700f897d3bf54ffa732459968d6ed05bf
kienct <kienct@langtech.vn>
2026-04-10 17:29:18 +0700

    feat: update fish theme colors and add quicker.nvim plugin

--- fish/.config/fish/conf.d/fish_frozen_theme.fish
+++ fish/.config/fish/conf.d/fish_frozen_theme.fish
@@ -5,36 +5,36 @@
 #     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
 # for appropriate commands to add to ~/.config/fish/config.fish instead.
 
-set --global fish_color_autosuggestion 9ca0b0
-set --global fish_color_cancel d20f39
-set --global fish_color_command 1e66f5
-set --global fish_color_comment 8c8fa1
-set --global fish_color_cwd df8e1d
-set --global fish_color_cwd_root 
-set --global fish_color_end fe640b
-set --global fish_color_error d20f39
-set --global fish_color_escape e64553
+set --global fish_color_autosuggestion 707A8C
+set --global fish_color_cancel --reverse
+set --global fish_color_command 5CCFE6
+set --global fish_color_comment 5C6773
+set --global fish_color_cwd 73D0FF
+set --global fish_color_cwd_root red
+set --global fish_color_end F29E74
+set --global fish_color_error FF3333
+set --global fish_color_escape 95E6CB
 set --global fish_color_gray 9ca0b0
-set --global fish_color_history_current 
-set --global fish_color_host 1e66f5
-set --global fish_color_host_remote 40a02b
+set --global fish_color_history_current --bold
+set --global fish_color_host --reset
+set --global fish_color_host_remote yellow
 set --global fish_color_keyword 8839ef
-set --global fish_color_normal 4c4f69
-set --global fish_color_operator ea76cb
+set --global fish_color_normal CBCCC6
+set --global fish_color_operator FFCC66
 set --global fish_color_option 40a02b
-set --global fish_color_param dd7878
-set --global fish_color_quote 40a02b
-set --global fish_color_redirection ea76cb
-set --global fish_color_search_match --background=ccd0da
-set --global fish_color_selection --background=ccd0da
-set --global fish_color_status d20f39
-set --global fish_color_user 179299
-set --global fish_color_valid_path 
-set --global fish_pager_color_completion 4c4f69
-set --global fish_pager_color_description 9ca0b0
-set --global fish_pager_color_prefix ea76cb
-set --global fish_pager_color_progress 9ca0b0
-set --global fish_pager_color_selected_background 
+set --global fish_color_param CBCCC6
+set --global fish_color_quote BAE67E
+set --global fish_color_redirection D4BFFF
+set --global fish_color_search_match --bold --background=FFCC66
+set --global fish_color_selection --bold --background=FFCC66
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_completion --reset
+set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_prefix --bold --underline=single
+set --global fish_pager_color_progress brwhite --bold --background=cyan
+set --global fish_pager_color_selected_background --background=FFCC66
 set --global fish_pager_color_selected_completion 
 set --global fish_pager_color_selected_description 
 set --global fish_pager_color_selected_prefix 
---
+++ nvim/.config/nvim/lua/plugins/quicker.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/quicker.nvim',
+  ft = "qf",
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {
+    buflisted = true,
+    number = true,
+    relativenumber = true,
+    signcolumn = "auto",
+    winfixheight = true,
+    wrap = false,
+  },
+}
